4263ca0e6166af2a9eb46d34817bfc5f9e85424c
Nigel Grech <nigelgrech.ng@gmail.com>
2021-11-09 10:39:07 +0100

    First draft for the linear layer

--- src/layers.py
+++ src/layers.py
@@ -1,32 +1,30 @@
 """Module for Layer classes for neural nets."""
 import numpy as np
 
-class FullyConnectedLayer():
-    """Fully connected or dense layer of the network."""
-
-    def __init__(self, inputs, depth) -> None:
-        """
-        Any fully connected layer in the networks
+class LinearLayer():
+    """Linear transformation layer of the type o = ixW + b,
+    
+    where I is the incoming vector, W is the layers weight matrix, b is bias vector and o is the dot product of the 
+    i and W plus the bias
+    
+    Args:
+        in_features (int): the size of the input features 
+        out_features (int): the size of the output features
         
-        Args:
-            inputs (int) : number of inputs the layer should expect
-        """
-        self.n_neurons = depth
-        self.weights = np.random.randn(input, depth)
-        self.bias = np.zeros((1, depth))
+    Attributes:
+        weights (np_array) numpy array of in_features x n_neurons
+        biases (np_array) numpy array of 1 x n_neurons
+    """
+
+    def __init__(self, in_features, out_features) -> None:
+        # initializing weights and biases 
+        self.weights = np.random.normal(0.0, np.sqrt(2/in_features), (in_features, out_features))
+        self.bias = np.zeros((1, out_features))
 
     def forward(self, inputs):
-        """
-        Forward pass of network
-
-        Args:
-            inputs (np.array) vector of input values 
-        """
-
+        # Storing output for backpropagation 
         self.output = np.dot(inputs, self.weights) + self.bias
-
         return self.output
-
 
 
 class Dropout():
