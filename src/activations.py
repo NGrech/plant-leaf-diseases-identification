f7f9be5a75ab84ef904a48b1f4a0d49b8025a057
Nigel Grech <nigelgrech.ng@gmail.com>
2021-11-09 11:22:34 +0100

    Add Softmax activation function

--- src/activations.py
+++ src/activations.py
@@ -6,5 +6,11 @@ class ReLU:
     def forward(x):
         return np.maximum(x, 0)
 
-class Softmax():
-    pass
+class Softmax:
+    """Applies Softmax function to input matrix."""
+    def forward(x):
+        # exponenets of each value
+        exp_vals = np.exp(x - np.max(x, axis=1, keepdims=True))
+        exp_sum = np.sum(exp_vals, axis=1, keepdims=True)
+        # Normalization to get the proabilities 
+        return exp_vals/exp_sum
