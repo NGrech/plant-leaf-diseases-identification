a5060889008bdbe272f241f5b77986a180fd8fb4
Nigel Grech <nigelgrech.ng@gmail.com>
2021-11-10 10:06:35 +0100

    Add implementation one hot encoder function

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,7 @@
 """Modules for common utility functions."""
+
+def one_hot_encode_index(y, n):
+    one_hot = np.zeros((len(y), n))
+    for _y, oh in zip(y, one_hot):
+        oh[_y] = 1
+    return one_hot
